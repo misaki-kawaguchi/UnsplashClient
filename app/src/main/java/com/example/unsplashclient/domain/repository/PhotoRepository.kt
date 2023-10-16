a2679623968a3dbf4d70f57e0cea8400ad52e895
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-10-16 23:29:39 +0900

    PhotoRepositoryの作成 (#5)

---
+++ app/src/main/java/com/example/unsplashclient/domain/repository/PhotoRepository.kt
@@ -0,0 +1,9 @@
+package com.example.unsplashclient.domain.repository
+
+import com.example.unsplashclient.data.remote.SearchPhotosResultDto
+
+
+interface PhotoRepository {
+
+  suspend fun searchPhotos(query: String): SearchPhotosResultDto
+}
