16527ee967a6ecb424284dfaa68145e8d4e59dab
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-10-21 21:44:15 +0900

    Repositoryに画像詳細取得関数を追加

--- app/src/main/java/com/example/unsplashclient/domain/repository/PhotoRepository.kt
+++ app/src/main/java/com/example/unsplashclient/domain/repository/PhotoRepository.kt
@@ -1,9 +1,12 @@
 package com.example.unsplashclient.domain.repository
 
+import com.example.unsplashclient.data.remote.PhotoDetailDto
 import com.example.unsplashclient.data.remote.SearchPhotosResultDto
 
 
 interface PhotoRepository {
 
   suspend fun searchPhotos(query: String): SearchPhotosResultDto
+
+  suspend fun getPhotoById(photoId: String): PhotoDetailDto
 }
