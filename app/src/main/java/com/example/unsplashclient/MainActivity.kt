9e6a7270cb24161d0c6115bffd23012e0a26192d
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-10-29 19:28:29 +0900

    NavHostを追加

--- app/src/main/java/com/example/unsplashclient/MainActivity.kt
+++ app/src/main/java/com/example/unsplashclient/MainActivity.kt
@@ -6,7 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.unsplashclient.presentation.ScreenRoute
 import com.example.unsplashclient.presentation.ui.theme.UnsplashClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,6 +21,22 @@ class MainActivity : ComponentActivity() {
       UnsplashClientTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+          val navController = rememberNavController()
+          NavHost(
+            navController = navController,
+            startDestination = ScreenRoute.SearchPhotosScreen.route,
+          ) {
+            // 画像検索画面
+            composable(route = ScreenRoute.SearchPhotosScreen.route) {
+              // TODO
+              Text(text = "画像検索画面")
+            }
+
+            // 画像詳細表示画面
+            composable(route = ScreenRoute.PhotoDetailScreen.route) {
+              // TODO
+            }
+          }
         }
       }
     }
